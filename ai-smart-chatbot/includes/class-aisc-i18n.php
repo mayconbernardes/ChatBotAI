061455d4201d171ff6cc30e691c214f0d532bbfe
mayconbernardes <mayconwin@hotmail.com>
2026-04-27 13:26:50 +0200

    Fix i18n class parse error

--- ai-smart-chatbot/includes/class-aisc-i18n.php
+++ ai-smart-chatbot/includes/class-aisc-i18n.php
@@ -18,38 +18,6 @@ class AISC_i18n {
     private function get_locale() {
         $locale = get_locale();
         
-        $available = array('en_US', 'fr_FR', 'pt_BR');
-        
-        foreach ($available as $lang) {
-            if (strpos($locale, substr($lang, 0, 2)) === 0) {
-                return $lang;
-            }
-        }
-        
-        return 'en_US';
-    }
-
-private function load_translations() {
-        $lang_files = array(
-            'en_US' => plugin_dir_path(__FILE__) . '../languages/en_US.po',
-            'fr_FR' => plugin_dir_path(__FILE__) . '../languages/fr_FR.po',
-            'pt_BR' => plugin_dir_path(__FILE__) . '../languages/pt_BR.po',
-            'es_ES' => plugin_dir_path(__FILE__) . '../languages/es_ES.po',
-            'zh_CN' => plugin_dir_path(__FILE__) . '../languages/zh_CN.po',
-            'ru_RU' => plugin_dir_path(__FILE__) . '../languages/ru_RU.po',
-            'de_DE' => plugin_dir_path(__FILE__) . '../languages/de_DE.po',
-        );
-
-        foreach ($lang_files as $locale => $file) {
-            if (file_exists($file)) {
-                $this->translations[$locale] = $this->parse_po_file($file);
-            }
-        }
-    }
-
-    private function get_locale() {
-        $locale = get_locale();
-        
         $available = array(
             'en_US' => array('en', 'en_US'),
             'fr_FR' => array('fr', 'fr_FR'),
@@ -70,6 +38,22 @@ private function load_translations() {
         
         return 'en_US';
     }
+
+    private function load_translations() {
+        $lang_files = array(
+            'en_US' => plugin_dir_path(__FILE__) . '../languages/en_US.po',
+            'fr_FR' => plugin_dir_path(__FILE__) . '../languages/fr_FR.po',
+            'pt_BR' => plugin_dir_path(__FILE__) . '../languages/pt_BR.po',
+            'es_ES' => plugin_dir_path(__FILE__) . '../languages/es_ES.po',
+            'zh_CN' => plugin_dir_path(__FILE__) . '../languages/zh_CN.po',
+            'ru_RU' => plugin_dir_path(__FILE__) . '../languages/ru_RU.po',
+            'de_DE' => plugin_dir_path(__FILE__) . '../languages/de_DE.po',
+        );
+
+        foreach ($lang_files as $locale => $file) {
+            if (file_exists($file)) {
+                $this->translations[$locale] = $this->parse_po_file($file);
+            }
         }
     }
 
